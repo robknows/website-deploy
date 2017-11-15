2b08d789b41a5aa3503d27848ca75ed47f22abec
Rob <robmoore121@gmail.com>
2017-11-15 18:00:51 +0000

    Can now close a webserver from any user

--- my-boring-website/down.sh
+++ my-boring-website/down.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill $(ps -u root f | grep "$QHOME/l32/q up.q" | grep -v "grep" | head -1 | awk '{print $1;}')
+kill $(ps -u $USER f | grep "$QHOME/l32/q up.q" | grep -v "grep" | head -1 | awk '{print $1;}')
 exit 0
