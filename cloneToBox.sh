280121cc9563764f2dae9c2064e14f4a743e4549
Rob <robmoore121@gmail.com>
2017-11-17 17:03:32 +0000

    Moves .gitignore from folder after copying to box. Clears house before copying

--- cloneToBox.sh
+++ cloneToBox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 serverfiles=$(cat SERVER_FILES)
 remotedir="serverfiles"
+./runOnBox.sh "rm -rf ./*"
 ./copyToBox.sh $serverfiles $remotedir
-./runOnBox.sh "mv $remotedir/* . && rm -d $remotedir"
+./runOnBox.sh "mv $remotedir/* . && mv $remotedir/.gitignore . && rm -d $remotedir"
 exit 0
