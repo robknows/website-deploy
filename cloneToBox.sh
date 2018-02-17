a4feb955234c63ddb5f86f8a0219af55db5cd2d7
Rob <robmoore121@gmail.com>
2018-02-17 14:51:49 +0000

    Clear out www directory now that it's www running the show

--- cloneToBox.sh
+++ cloneToBox.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 serverfiles=$(cat SERVER_FILES)
 remotedir="serverfiles"
-./runOnBox.sh "rm -rf ./*"
-./copyToBox.sh $serverfiles $remotedir
-./runOnBox.sh "mv $remotedir/* . && mv $remotedir/.gitignore . && rm -d $remotedir"
-./runOnBox.sh "sudo mv /home/ubuntu/* /home/www/"
-./runOnBox.sh "sudo chown -R www:www /home/www/*"
+./runOnBox.sh "sudo rm -rf /home/www/*" # Clean www home
+./copyToBox.sh $serverfiles $remotedir  # Copy the files over
+./runOnBox.sh "mv $remotedir/* . && mv $remotedir/.gitignore . && rm -d $remotedir" # Pull out the files
+./runOnBox.sh "sudo mv /home/ubuntu/* /home/www/" # Move everything into www's home folder
+./runOnBox.sh "sudo chown -R www:www /home/www/*" # Make sure www owns all of the server files
 exit 0
